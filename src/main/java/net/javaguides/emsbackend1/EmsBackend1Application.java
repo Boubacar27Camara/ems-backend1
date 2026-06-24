a9a6bd1f24137ecc03e50b50b8a5e8a7b6c117ef
Boubacar <saloumc147@gmail.com>
2026-06-24 12:45:56 +0000

    Gestion des employees

---
+++ src/main/java/net/javaguides/emsbackend1/EmsBackend1Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.emsbackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsBackend1Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/javaguides/emsbackend1/EmsBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.emsbackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
